22d503c9df608c71178c080d61d7bff681c73aa6
Rapsac <caspar.kersting@gmail.com>
2023-12-09 22:05:48 +0100

    first config

---
+++ src/main/java/net/rapsac/scrollablesavedhotbar/ScrollableSavedHotbarClient.java
@@ -0,0 +1,10 @@
+package net.rapsac.scrollablesavedhotbar;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ScrollableSavedHotbarClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
