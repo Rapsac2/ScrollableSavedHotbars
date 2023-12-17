a86d2588b3a86784c0967b7fb53c6c1b7df59a94
Rapsac <caspar.kersting@gmail.com>
2023-12-17 21:27:03 +0100

    startet

--- src/main/java/net/rapsac/scrollablesavedhotbar/ScrollableSavedHotbar.java
+++ src/main/java/net/rapsac/scrollablesavedhotbar/ScrollableSavedHotbar.java
@@ -1,34 +1,35 @@
 package net.rapsac.scrollablesavedhotbar;
 
-//import net.fabricmc.api.ModInitializer;
-//import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-//import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
-//import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.system.windows.INPUT;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import org.spongepowered.asm.mixin.Final;
-//import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Shadow;
 
-//import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.GameOptions;
 
-//import net.minecraft.client.option.KeyBinding;
-//import net.minecraft.client.util.InputUtil;
-//import net.minecraft.client.util.InputUtil.Key;
-//import net.minecraft.text.Text;
-//import net.minecraft.text.PlainTextContent.Literal;
-//import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.util.InputUtil.Key;
+import net.minecraft.text.Text;
+import net.minecraft.text.PlainTextContent.Literal;
+import net.minecraft.client.MinecraftClient;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
-//import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-//import net.minecraft.client.MinecraftClient;
-//import net.minecraft.client.option.KeyBinding;
-//import net.minecraft.client.util.InputUtil;
-//import net.minecraft.entity.player.PlayerInventory;
-//import org.jetbrains.annotations.Nullable;
-//import org.lwjgl.glfw.GLFW;
-//import net.rapsac.scrollablesavedhotbar.mixin.KeyMixin;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerInventory;
+import org.jetbrains.annotations.Nullable;
+import org.lwjgl.glfw.GLFW;
+import net.rapsac.scrollablesavedhotbar.mixin.KeyMixin;
 
 
 public class ScrollableSavedHotbar implements ModInitializer { // ModInitializer ( ClientModInitializer idt falsch und wirft eine Fehler meldung)
@@ -40,19 +41,47 @@ public class ScrollableSavedHotbar implements ModInitializer { // ModInitializer
 	//public KeyBinding somethingkey = MinecraftClient.getInstance().options.jumpKey;
 	//public KeyBinding keyBind = MinecraftClient.options.jumpKey
 	
-	//public static GameOptions jumpKeyBind;
+	public static GameOptions jumpKeyBind; // = KeyMixin.options;
+	public static KeyBinding ghj;
+	private static int index;
+	private static Key[] key;
+	private static int scrollAmountInt; 
 
-	//public static Boolean Scrolling(double scrollAmount) {
+	public static Boolean Scrolling(double scrollAmount) {
 		
-		
-		//if(jumpKeyBind.jumpKey.isPressed()){
-		//	LOGGER.info("scroll");
-		//	return true;
-		//} else {
-		//	return false;
-		//}
-		
-	//}
+		//if(jumpKeyBind != null){
+			
+			if(InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 342)){ //342 müsste Left_alt sein   88 ist x
+			//if(jumpKeyBind.jumpKey.isPressed()){
+			//if(KeyMixin.sprung.isPressed()){
+				scrollAmountInt = (int)scrollAmountInt;
+				KeyBinding.setKeyPressed(key[index], false);
+				LOGGER.info("scroll");
+				LOGGER.info("scrollAmoount "+scrollAmount);
+				LOGGER.info("scrollAmountInt"+scrollAmountInt);
+				index = index - scrollAmountInt;
+				if(index == 0 && scrollAmountInt > 0){
+					index = 8;
+					LOGGER.info("index von 0 auf 8");
+				}else if(index == 8 && scrollAmount < 0){
+					index = 0;
+				}
+				
+				LOGGER.info(index + "");
+				KeyBinding.setKeyPressed(key[index], true);
+				
+				return true;
+			} else {//reset ob taste gedrückt ist
+				if(index != -1){
+					KeyBinding.setKeyPressed(key[index], false);
+					index = -1;
+					LOGGER.info("set index -1");
+				}
+				LOGGER.info("alt is not pressed");
+				//KeyBinding.setKeyPressed(key[index], false);
+				return false;
+			}
+	}
 
 	
 
@@ -98,7 +127,17 @@ public class ScrollableSavedHotbar implements ModInitializer { // ModInitializer
 	@Override
 	public void onInitialize(){
 			LOGGER.info("-------------------Rapsac_______--------------");
+			//jumpKeyBind = KeyMixin.options;
+			//ghj = GameOptions.jumpKey;
+			//key = InputUtil.fromTranslationKey(GLFW_KEY_1);
+			key = new Key[9];
+			//key[0] = InputUtil.fromTranslationKey("key.keyboard.0");
+			for(int i = 0; i < 9; i++){
+				LOGGER.info(i + "");
+				key[i] = InputUtil.fromTranslationKey("key.keyboard." + (i+1));	
+			}
+			index = 0;
+			//key = InputUtil.fromTranslationKey("key.keyboard.space");
+			LOGGER.info("option");
 	}
-
-	
 }
